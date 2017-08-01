690a5a009cdf2f5d8f9e165252712fdad9a2978f
unholydeath785 <ivansonofcoul@gmail.com>
2017-07-31 18:14:19 -0700

    made moving platforms

---
+++ Club Fair Game/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+	public float moveDelta = 5f;
+	public float speed = 5f;
+	private float baseX = 0;
+
+	void Awake() {
+		baseX = transform.position.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float dX = Mathf.PingPong (Time.time * speed, moveDelta);
+		transform.position = new Vector3 (baseX + dX, transform.position.y, transform.position.z);
+	}
+}
